cce846258d9530a8e696eef787892a45d83ca747
Shathees1234 <shatheesg833@gmail.com>
2025-12-19 13:01:44 +0530

    Initialize App component with product management layout

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ProductList from "./ProductList";
+import AddProduct from "./AddProduct";
+
+function App() {
+  return (
+    <div>
+      <h1>AI-Driven E-Commerce Product Management</h1>
+      <AddProduct />
+      <ProductList />
+    </div>
+  );
+}
+
+export default App;
